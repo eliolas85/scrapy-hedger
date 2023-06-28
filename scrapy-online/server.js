ff7e81af89fb862314be83b777099db2fab3426e
eliolas85 <pierpaolomolinaro.design@gmail.com>
2023-06-28 02:06:56 +0200

    tutti gli asset vengono registrati correttamente
    step successivo: organizzare in modo più efficiente la struttura del codice.

--- scrapy-online/server.js
+++ scrapy-online/server.js
@@ -1,6 +1,4 @@
-// bid = prezzo di acquisto
-// ask = prezzo di vendita
-
+const calcolaValori = require("./calcolaValori");
 const http = require("http");
 const express = require("express");
 const WebSocket = require("ws");
@@ -31,7 +29,7 @@ function calculateInverseAverageBid(assetName, assetsMap) {
 }
 
 const app = express();
-process.setMaxListeners(20);
+process.setMaxListeners(100);
 app.use(cors());
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ noServer: true });
@@ -141,9 +139,7 @@ app.get("/calculate", (req, res) => {
           const spread2 = Math.abs(broker2Data.bid - broker2Data.ask);
 
           // Calcola il valore del pip in base alla tipologia dell'asset
-          let pipValue1, pipValue2;
-          let valore_lotto_broker1 = 100000 / 30;
-          let valore_lotto_broker2 = 100000 / 30;
+
           let usdEur = 0;
           let eurNzd = 0;
           let gbpEur = 0;
@@ -162,123 +158,27 @@ app.get("/calculate", (req, res) => {
 
           // Usiamo un'istruzione switch per gestire tutte le diverse coppie di valute:
 
-          let leverage = 30;
-          switch (asset) {
-            case "GBPUSD":
-              //ok
-              leverage = 30;
-              pipValue1 = (0.0001 * 100000) / eurUsdBid_1;
-              pipValue2 = (0.0001 * 100000) / eurUsdBid_2;
-              valore_lotto_broker1 = (eurGbp * 100000) / leverage;
-              valore_lotto_broker2 = (eurGbp * 100000) / leverage;
-              break;
-            case "USDJPY":
-              //ok
-              leverage = 30;
-              pipValue1 = (0.01 * 100000) / eurJpyBid_1;
-              pipValue2 = (0.01 * 100000) / eurJpyBid_2;
-              valore_lotto_broker1 = (usdEur * 100000) / leverage;
-              valore_lotto_broker2 = (usdEur * 100000) / leverage;
-              break;
-            case "AUDUSD":
-              //ok
-              leverage = 20;
-              pipValue1 = (0.0001 * 100000) / eurUsdBid_1;
-              pipValue2 = (0.0001 * 100000) / eurUsdBid_2;
-              valore_lotto_broker1 = (eurAud * 100000) / leverage;
-              valore_lotto_broker2 = (eurAud * 100000) / leverage;
-
-              break;
-            case "NZDUSD":
-              // ok
-              leverage = 20;
-              pipValue1 = (0.0001 * 100000) / eurUsdBid_1;
-              pipValue2 = (0.0001 * 100000) / eurUsdBid_2;
-              valore_lotto_broker1 = (eurNzd * 100000) / leverage / eurUsdBid_1;
-              valore_lotto_broker2 = (eurNzd * 100000) / leverage / eurUsdBid_2;
-
-              break;
-            case "USDCAD":
-              //ok
-              leverage = 30;
-              pipValue1 = (0.0001 * 100000) / eurCadBid_1;
-              pipValue2 = (0.0001 * 100000) / eurCadBid_2;
-              valore_lotto_broker1 = (usdEur * 100000) / leverage;
-              valore_lotto_broker2 = (usdEur * 100000) / leverage;
-
-              break;
-            case "EURCHF":
-              leverage = 30;
-              pipValue1 = (0.0001 * 100000) / broker1Data.bid;
-              pipValue2 = (0.0001 * 100000) / broker2Data.bid;
-              valore_lotto_broker1 = 100000 / leverage;
-              valore_lotto_broker2 = 100000 / leverage;
-
-              break;
-            case "EURJPY":
-              //ok
-              leverage = 30;
-              pipValue1 = (0.01 * 100000) / broker1Data.bid;
-              pipValue2 = (0.01 * 100000) / broker2Data.bid;
-              valore_lotto_broker1 = 100000 / leverage;
-              valore_lotto_broker2 = 100000 / leverage;
-              break;
-
-            case "EURUSD":
-              //ok
-              leverage = 30;
-              pipValue1 = (0.0001 / broker1Data.bid) * 100000;
-              pipValue2 = (0.0001 / broker2Data.bid) * 100000;
-              valore_lotto_broker1 = 100000 / leverage;
-              valore_lotto_broker2 = 100000 / leverage;
-
-              break;
-            case "NATGAS":
-              // no
-              leverage = 10;
-              valore_lotto_broker1 =
-                (broker1Data.bid * 10000) / leverage / eurUsdBid_1;
-              valore_lotto_broker2 =
-                (broker2Data.bid * 10000) / leverage / eurUsdBid_2;
-              broker1PipValue = 0.001 * 10000;
-              broker2PipValue = 0.001 * 10000;
-              break;
-
-            case "EURNZD":
-              leverage = 20;
-              pipValue1 = (0.0001 * 100000) / broker1Data.bid;
-              pipValue2 = (0.0001 * 100000) / broker2Data.bid;
-              valore_lotto_broker1 = 100000 / leverage;
-              valore_lotto_broker2 = 100000 / leverage;
-              break;
-
-            case "EURGBP":
-              leverage = 30;
-              pipValue1 = (0.0001 * 100000) / broker1Data.bid;
-              pipValue2 = (0.0001 * 100000) / broker2Data.bid;
-              valore_lotto_broker1 = 100000 / leverage;
-              valore_lotto_broker2 = 100000 / leverage;
-              break;
-
-            case "EURAUD":
-              leverage = 20;
-              pipValue1 = (0.0001 * 100000) / broker1Data.bid;
-              pipValue2 = (0.0001 * 100000) / broker2Data.bid;
-              valore_lotto_broker1 = 100000 / leverage;
-              valore_lotto_broker2 = 100000 / leverage;
-              break;
-
-            case "CRUDEOIL":
-              leverage = 10;
-              pipValue1 = (0.01 * 1000) / eurUsdBid_1;
-              pipValue2 = (0.01 * 1000) / eurUsdBid_2;
-              valore_lotto_broker1 = (10 * broker1Data.bid) / eurUsdBid_1;
-              valore_lotto_broker2 = (10 * broker2Data.bid) / eurUsdBid_2;
-              break;
-
-            default:
-              console.log("Asset non supportato: " + asset);
-          }
+          let {
+            leverage,
+            pipValue1,
+            pipValue2,
+            valore_lotto_broker1,
+            valore_lotto_broker2,
+          } = calcolaValori(
+            asset,
+            eurUsdBid_1,
+            eurUsdBid_2,
+            eurGbp,
+            eurJpyBid_1,
+            eurJpyBid_2,
+            eurAud,
+            eurNzd,
+            eurCadBid_1,
+            eurCadBid_2,
+            usdEur,
+            broker1Data,
+            broker2Data
+          );
 
           let assetId = results.length + 1;
           results.push({
@@ -315,17 +215,6 @@ server.listen(8080, function listening() {
 });
 
 const updateFrequency = 5 * 60 * 1000; // 5 minuti in millisecondi;
-const assets = [
-  "NZDUSD",
-  "GBPUSD",
-  "EURUSD",
-  "EURNZD",
-  "EURGBP",
-  "EURAUD",
-  "AUDUSD",
-  "NATGAS",
-];
-
 const etoroAssets = [
   "EURUSD", //ok
   "NZDUSD", //ok
@@ -340,7 +229,7 @@ const etoroAssets = [
   "USDCAD", //ok
   "EURCHF", //ok
   "EURJPY", //ok
-  "OIL", //no
+  "OIL", //ok
 ];
 const plus500Assets = [
   "EURUSD",
